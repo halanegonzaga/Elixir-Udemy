9adc43c6d07f3b9b4bb035d0eda23b043a47d348
Halane Gonzaga <halanesantos.g@gmail.com>
2019-02-11 06:42:59 -0200

    Buscando uma fatura

--- fatura/lib/fatura.ex
+++ fatura/lib/fatura.ex
@@ -24,4 +24,13 @@ defmodule Fatura do
     Enum.sort(faturas)
   end
 
+  @doc """
+    Verifica se fatura existe
+    ## Exemplos
+      iex> Fatura.fatura_existe?(Fatura.criar_fatura(["Telefone", "Agua", "Luz"]), "Agua")
+      true
+  """
+  def fatura_existe?(faturas, fatura) do
+    Enum.member?(faturas,fatura)
+  end
 end
