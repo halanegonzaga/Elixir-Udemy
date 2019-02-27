958d5b406a792a333f3757f6869c373c56cff0e7
Halane Gonzaga <halanesantos.g@gmail.com>
2019-02-27 10:09:34 -0300

    Operador pipe, escrita e leitura de arquivos

--- fatura/lib/fatura.ex
+++ fatura/lib/fatura.ex
@@ -6,12 +6,12 @@ defmodule Fatura do
 
   @doc """
     Ao receber `fatura` retorna um Array de faturas
-      ## Exemplos
+    ## Exemplos
       iex> Fatura.criar_faturas(["Telefone", "Agua", "Luz"], [1])
       ["Telefone", "Agua", "Luz"]
   """
   def criar_faturas(faturas, vencimentos) do
-    
+
     # Percorrendo Lista
     for _vencimento <- vencimentos, fatura <- faturas do
       # String template
@@ -24,10 +24,11 @@ defmodule Fatura do
     # List.flatten(lista_de_faturas)
 
   end
-  
+
   @doc """
     Ordenar os itens de faturas
-      ## Exemplos
+
+    ## Exemplos
       iex> Fatura.orderna_fatura(["Telefone", "Agua", "Luz"])
       ["Agua", "Luz", "Telefone"]
   """
@@ -37,18 +38,19 @@ defmodule Fatura do
 
   @doc """
     Verifica se fatura existe
+
     ## Exemplos
       iex> Fatura.fatura_existe?(["Telefone", "Agua", "Luz"], "Agua")
       true
   """
   def fatura_existe?(faturas, fatura) do
-    Enum.member?(faturas,fatura)
+    Enum.member?(faturas, fatura)
   end
 
   @doc """
     Indicar faturas a pagar
 
-    ##Exemplo
+    ## Exemplos
       iex> Fatura.faturas_a_pagar(faturas,1)
       {["Telefone"], ["Agua", "Luz"]}
 
@@ -63,5 +65,71 @@ defmodule Fatura do
   """
   def  faturas_a_pagar(faturas, quantidade) do
     Enum.split(faturas, quantidade)
+  end
+
+
+  @doc """
+    Salvar em um arquivo as faturas registradas
+
+    ##Exemplo
+      iex> fatura = Fatura.criar_faturas(["Telefone", "Agua", "Luz"], [1])
+      ["Telefone", "Agua", "Luz"]
+      iex> fatura
+      ["Telefone", "Agua", "Luz"]
+      iex> Fatura.salva_fatura("lib/fatura.txt", fatura)
+      :ok
+
+  """
+
+  def salva_fatura(nome_arquivo, faturas) do
+    binary = :erlang.term_to_binary(faturas)
+    File.write(nome_arquivo, binary)
+  end
+
+  @doc """
+    Leitura dos arquivos da fatura
+
+    ##Exemplo
+      iex> Fatura.load("lib/fatura.txt")
+      ["Telefone", "Agua", "Luz"]
+
+      iex> Fatura.load("test")
+      "Não foi possivel ler o arquivo"
+
+  """
+
+  def load(nome_arquivo) do
+    #    {status, binario} = File.read(nome_arquivo)
+    #
+    #    # Evitar IFs
+    #    case status do
+    #      :ok -> :erlang.binary_to_term binario
+    #      :error -> "Não foi possivel ler o arquivo"
+    #    end
+
+    case File.read(nome_arquivo) do
+      {:ok, binario} -> :erlang.binary_to_term binario
+      {:error, _erro} -> "Não foi possivel ler o arquivo"
+    end
+
+  end
+
+  @doc """
+    Exemplo de implementação de pagamento de faturas com operador pipe
+
+    ##Exemplo
+      iex> Fatura.pagar_faturas(["Telefone", "Agua"], [5,10], 1)
+      {["Agua"], ["Agua", "Telefone", "Telefone"]}
+
+  """
+
+  def pagar_faturas(faturas, vencimentos, quantidade) do
+
+    #Operador pipe
+
+    criar_faturas(faturas, vencimentos)
+    |> orderna_fatura
+    |> faturas_a_pagar(quantidade)
+
   end
 end
--- fatura/test/fatura_test.exs
+++ fatura/test/fatura_test.exs
@@ -2,17 +2,17 @@ defmodule FaturaTest do
   use ExUnit.Case
   doctest Fatura
 
-  test "Criando fatura" do
-    faturas = Fatura.criar_fatura(["Agua", "Luz", "Phone"])
-    assert faturas == ["Agua", "Luz", "Phone"]
-  end
-
-  test "Ordenando Fatura" do
-    faturas = Fatura.criar_fatura(["Agua", "Rua", "Luz"])
-    refute Fatura.orderna_fatura(faturas) == ["Agua", "Rua", "Luz"]
-  end
-
-  test "Existe Fatura" do
-    assert Fatura.fatura_existe?(Fatura.criar_fatura(["Telefone", "Agua", "Luz"]), "Agua") == true
-  end
+#  test "Criando fatura" do
+#    faturas = Fatura.criar_fatura(["Agua", "Luz", "Phone"])
+#    assert faturas == ["Agua", "Luz", "Phone"]
+#  end
+#
+#  test "Ordenando Fatura" do
+#    faturas = Fatura.criar_fatura(["Agua", "Rua", "Luz"])
+#    refute Fatura.orderna_fatura(faturas) == ["Agua", "Rua", "Luz"]
+#  end
+#
+#  test "Existe Fatura" do
+#    assert Fatura.fatura_existe?(Fatura.criar_fatura(["Telefone", "Agua", "Luz"]), "Agua") == true
+#  end
 end
